d22461b502568c589487c52009619fabac383ca3
Mecha Temesgen <ililnaafbarihe94@gmail.com>
2023-11-21 18:18:10 +0300

    Update commit

--- main.py
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-name = input("Enter First Name: ")
-=======
-name = input("Enter First Name: ")
->>>>>>> 03c182b8fc9d2095208ae239563f3416fe002016
-print("Hello " + name + ", Welcome! How are you doing " + name +"?" )
